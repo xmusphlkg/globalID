993faedc04d4748f5a173e5db6bd90ec9c5ba00e
xmusphlkg <lkg1116@outlook.com>
2024-07-09 15:20:31 +0000

    add map

---
+++ Website/themes/hugo-theme-dashboard/static/js/support_map.js
@@ -0,0 +1,12 @@
+var data = [
+  { "name": "CHN", "var": "Supported" },
+  { "name": "THA", "var": "Scheduled" },
+  { "name": "AUS", "var": "Scheduled" },
+  { "name": "USA", "var": "Scheduled" },
+  { "name": "GBR", "var": "Scheduled" },
+  { "name": "KOR", "var": "Scheduled" },
+  { "name": "NZL", "var": "Scheduled" },
+  { "name": "SWE", "var": "Scheduled" },
+  { "name": "JPN", "var": "Scheduled" },
+  { "name": "SGP", "var": "Scheduled" },
+];
